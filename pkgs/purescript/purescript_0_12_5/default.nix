5fc376c793151e31cb704f4fbbd193e9e82b16be
Tomasz Rybarczyk <paluho@gmail.com>
2019-04-23 12:37:06 +0200

    Use ghc8.6.4 for purs-0.12.4 and purs-0.12.5

--- pkgs/purescript/purescript_0_12_5/default.nix
+++ pkgs/purescript/purescript_0_12_5/default.nix
@@ -1,29 +1,16 @@
 { haskell }:
-
 let
   purescriptPkg = import ../mkPurescriptPkg.nix {
     version = "0.12.5";
     sha256 = "0dpn0v510lgzd9zqglqan4m8l7bf891psqmih147pnrmigmiaa39";
   };
-  haskellPackages = haskell.packages.ghc844.override {
+  haskellPackages = haskell.packages.ghc864.override {
     overrides = haskellPackagesNew: haskellPackagesOld: rec {
-      ChasingBottoms = haskellPackagesNew.callPackage ./libraries/ChasingBottoms.nix {};
-      hspec-core = haskellPackagesNew.callPackage ./libraries/hspec-core.nix {};
-      hspec-discover = haskellPackagesNew.callPackage ./libraries/hspec-discover.nix {};
-      hspec-meta = haskellPackagesNew.callPackage ./libraries/hspec-meta.nix {};
-      hspec = haskellPackagesNew.callPackage ./libraries/hspec.nix {};
       network = haskellPackagesNew.callPackage ./libraries/network.nix {};
-      optparse-applicative = haskellPackagesNew.callPackage ./libraries/optparse-applicative.nix {};
       purescript = haskellPackagesNew.callPackage purescriptPkg {};
-      psqueues = haskellPackagesNew.callPackage ./libraries/psqueues.nix {};
-      QuickCheck = haskellPackagesNew.callPackage ./libraries/QuickCheck.nix {};
-      simple-sendfile = haskellPackagesNew.callPackage ./libraries/simple-sendfile.nix {};
-      test-framework-quickcheck2 = haskellPackagesNew.callPackage ./libraries/test-framework-quickcheck2.nix {};
-      warp = haskell.lib.dontCheck (haskellPackagesNew.callPackage ./libraries/warp.nix {});
-      wai-extra = haskellPackagesNew.callPackage ./libraries/wai-extra.nix {};
-      websockets = haskellPackagesNew.callPackage ./libraries/websockets.nix {};
     };
   };
 in
   haskellPackages.purescript
+
 
